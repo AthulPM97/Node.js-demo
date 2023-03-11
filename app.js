a0e50fa1c0bc8cb00fae18a640a2f34ba4e4159e
athulpm <97athulpm@gmail.com>
2023-03-11 17:59:34 +0530

    added request and response to server

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Athul')
+    if(req.url === '/') {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>root</title></head>');
+        res.write('<body><h1>My first page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(req.url === '/home') {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(req.url === '/about') {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>About</title></head>');
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(req.url === '/node') {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Node</title></head>');
+        res.write('<body><h1>Welcome to my node js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 server.listen(4000);
