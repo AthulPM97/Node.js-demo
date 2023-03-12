f9eac4e73efd6d3ba9e9014da2b42e913c7606b0
athulpm <97athulpm@gmail.com>
2023-03-12 13:31:42 +0530

    Cleaned up code

--- ChatApp.js
+++ ChatApp.js
@@ -1,50 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    // read stored message
-    const storedMsg = fs.readFileSync("message.txt", "utf8", (err, data) => {
-      if (err) console.log(err);
-      return data;
-    });
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write("<body>");
-    res.write('<p>');
-    res.write(`${storedMsg}`);
-    res.write('</p>')
-    res.write(
-      '<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
-    );
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const requestHandler = require("./routes");
+
+const server = http.createServer(requestHandler);
 
 server.listen(3000);
