b170567b306c88825acbf2b96cb78f5014d4bd07
athulpm <97athulpm@gmail.com>
2023-03-15 12:27:01 +0530

    Installed express and added basic response

--- ChatApp.js
+++ ChatApp.js
@@ -1,7 +1,15 @@
-const http = require("http");
+const express = require('express');
 
-const requestHandler = require("./routes");
+const app = express();
 
-const server = http.createServer(requestHandler);
+app.use((req,res,next) => {
+    console.log('in a middleware');
+    next();
+})
 
-server.listen(3000);
+app.use((req,res,next) => {
+    console.log('in another middleware');
+    res.send('<h1>Welcome to Express!</h1>');
+})
+
+app.listen(3000);
