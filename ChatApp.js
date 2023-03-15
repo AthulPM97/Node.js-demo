b60998ed3bee4ac2a2e4b68f2c67cfe8f651854c
athulpm <97athulpm@gmail.com>
2023-03-15 20:08:13 +0530

    added and filtered routes for shop and admin

--- ChatApp.js
+++ ChatApp.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); // configuring body parser to not use extended query string library. Use normal library instead.
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='product'/><input type='number' name='size'/><button type='submit'>submit</button></form>"
-  );
-});
+app.use('/admin',adminRoutes);
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use('/shop', shopRoutes);
 
-app.use("/", (req, res, next) => {
-  console.log("in another middleware");
-  res.send("<h1>Welcome to Express!</h1>");
-});
+app.use((req,res,next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+})
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("running on port 3000");
+});
