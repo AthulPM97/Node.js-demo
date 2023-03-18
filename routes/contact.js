ca60260d4a6f52abb320b193cd27022c8451d5aa
athulpm <97athulpm@gmail.com>
2023-03-18 11:01:20 +0530

    added controllers for contact page

--- routes/contact.js
+++ routes/contact.js
@@ -2,15 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const contactController = require('../controllers/contact');
+
 const router = express.Router();
-const rootDir = require('../util/path');
 
-router.get('/contactus', (req,res,next) => {
-    res.sendFile(path.join(rootDir, 'views', 'contact-us.html'));
-});
+router.get('/contactus', contactController.getContactUs);
 
-router.post('/contactus/success', (req,res,next) => {
-    res.sendFile(path.join(rootDir, 'views', 'success.html'));
-});
+router.post('/contactus/success', contactController.getSuccess);
 
 module.exports = router;
